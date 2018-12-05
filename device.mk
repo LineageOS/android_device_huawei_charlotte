f87a897083c4971e4d071188581c14e0ea08bff3
LuK1337 <priv.luk@gmail.com>
2018-12-05 11:52:56 +0100

    charlotte: Inherit product_launched_with_o_mr1.mk

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/huawei/charlotte/charlotte-vendor.mk)
 
